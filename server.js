c763622830f5f22ef953ff7fa09eb40337e8d0df
Lautaro Rodriguez Barreiro <lautarorb@Lautaros-MacBook-Pro.local>
2025-09-12 22:26:23 -0300

    Fix ES modules compatibility

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 
-const { Server } = require('@modelcontextprotocol/sdk/server/index.js');
-const { StdioServerTransport } = require('@modelcontextprotocol/sdk/server/stdio.js');
-const {
+import { Server } from '@modelcontextprotocol/sdk/server/index.js';
+import { StdioServerTransport } from '@modelcontextprotocol/sdk/server/stdio.js';
+import {
   CallToolRequestSchema,
   ListToolsRequestSchema,
-} = require('@modelcontextprotocol/sdk/types.js');
-const http = require('http');
+} from '@modelcontextprotocol/sdk/types.js';
+import http from 'http';
 
 class TimeServer {
   constructor() {
